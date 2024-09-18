cedd0586dcb26ee5c0f308498c54422fc61daa37
sanjay-kumar-singh-73 <145865352+sanjay-kumar-singh-73@users.noreply.github.com>
2024-09-18 21:50:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var typed = new Typed(".loop",{
+    strings : ["Frontend Developer", "YouTuber","Backend Developer","Full Stack Developer"],
+
+    typeSpeed : 150,
+    backSpeed : 150,
+    looped : true
+})
+
+
+
+
+function Color(){
+    document.body.style.backgroundColor = 'black';
+}
